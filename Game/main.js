62fbaf3b817b3a5b0831f17fe3e79c70c979d440
Benoit Pelourdeau <benoit.pelourdeau@gmail.com>
2019-04-12 21:54:21 +0200

    The head moooves

--- Game/main.js
+++ Game/main.js
@@ -1,18 +1,72 @@
 const gameState = {};
 
-function create() {
-	this.add.text(20, 20, "Hello World");
-}
-
 const config = {
 	type: Phaser.WEBGL,
 	width: 600,
 	height: 600,
 	backgroundColor: 0xdda0dd,
 	parent: "game",
+	physics: {
+		default: 'arcade',
+			arcade: {
+            debug: true
+            }
+        },
 	scene: {
-		create
+		preload,
+		create,
+		update
 	}
 };
 
 const game = new Phaser.Game(config);
+
+function preload() {
+	this.load.image('snakehead', 'Game/assets/snakehead.png');
+}
+
+function create(){
+	gameState.snakeHead = this.physics.add.sprite(300, 300,'snakehead');
+	gameState.cursors = this.input.keyboard.createCursorKeys();
+	gameState.direct = "right";
+	gameState.speed = 100;//means x pixel/sec
+	gameState.snakeHead.setVelocityX(gameState.speed);
+	gameState.snakeHead.setCollideWorldBounds(true);
+	gameState.timedLoop = this.time.addEvent({ delay: Math.floor(20*1000/gameState.speed), callback: updateDirection, callbackScope: this, loop: true });
+  }
+  
+function updateDirection() {
+	if (gameState.direct === "right"){
+		gameState.snakeHead.setVelocityY(0);
+		gameState.snakeHead.setVelocityX(gameState.speed);
+	}
+	if (gameState.direct === "left"){
+		gameState.snakeHead.setVelocityY(0);
+		gameState.snakeHead.setVelocityX(-gameState.speed);
+	}
+	if (gameState.direct === "down"){
+		gameState.snakeHead.setVelocityX(0);
+		gameState.snakeHead.setVelocityY(gameState.speed);
+	}
+	if (gameState.direct === "up"){
+		gameState.snakeHead.setVelocityX(0);
+		gameState.snakeHead.setVelocityY(-gameState.speed);
+	}
+	
+}
+  
+function update(){
+	if (gameState.cursors.right.isDown){
+		gameState.direct = "right";
+	}
+	if (gameState.cursors.left.isDown){
+		gameState.direct = "left";
+	}
+	if (gameState.cursors.down.isDown){
+		gameState.direct = "down";
+	}
+	if (gameState.cursors.up.isDown){
+		gameState.direct = "up";
+	}
+}
+
